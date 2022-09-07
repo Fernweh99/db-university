62bab7dc5069958e2192ad7bdaa70f4428d9adb1
Fernweh99 <daniele.dimento@gmail.com>
2022-09-07 16:29:01 +0200

    second ex of join

--- join.sql
+++ join.sql
@@ -6,7 +6,13 @@ ON `degrees`.`id` = `students`.`degree_id`
 WHERE `degrees`.`name` = "Corso di Laurea in Economia";
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-
+SELECT `courses`.* 
+FROM `courses`
+JOIN `degrees`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `departments`
+ON `departments`.`id` = `degrees`.`department_id`
+WHERE `departments`.`name` = "dipartimento di neuroscienze";
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
